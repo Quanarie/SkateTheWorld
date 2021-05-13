b602e51a96dc455b0980ee460adb993a82cb6a69
sppeeedd <qustaine@gmail.com>
2021-05-13 19:43:19 +0300

    Added healthBar

--- Assets/Scripts/Skater/SkaterHealth.cs
+++ Assets/Scripts/Skater/SkaterHealth.cs
@@ -2,18 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SkaterHealth : MonoBehaviour
 {
     [SerializeField] private int health;
     [SerializeField] private GameObject damageTakenEffect;
     [SerializeField] private GameObject healEffect;
+    [SerializeField] private Image[] hearts;
+    [SerializeField] private Sprite fullHeart;
+    [SerializeField] private Sprite halfFullHeart;
+    [SerializeField] private Sprite emptyHeart;
 
     private int maxHealth;
 
     private void Start()
     {
         maxHealth = health;
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if ((i + 1) * 2 <= health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else if ((i + 1) * 2 == health + 1)
+            {
+                hearts[i].sprite = halfFullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+        }
     }
 
     public void TakeDamage(int damage)
