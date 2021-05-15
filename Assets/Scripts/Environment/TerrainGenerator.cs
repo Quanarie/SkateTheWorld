807f6ffd5db17a10cf75d68c25e3f78df7ab08b2
sppeeedd <qustaine@gmail.com>
2021-05-15 12:28:22 +0300

    Added deletion of the world behind, fixed cameraMovement so that player cant see deleted world

--- Assets/Scripts/Environment/TerrainGenerator.cs
+++ Assets/Scripts/Environment/TerrainGenerator.cs
@@ -12,15 +12,20 @@ public class TerrainGenerator : MonoBehaviour
     private float previousPositionX = 0;
     private float previousPositionY = 0;
     private float previousBGPosX = 0;
+    private List<GameObject> cells;
+    private List<GameObject> BGs;
 
     private void Start()
     {
+        cells = new List<GameObject>();
+        BGs = new List<GameObject>();
         previousPositionY = transform.position.y;
     }
 
     private void Update()
     {
         Generate();
+        Delete();
     }
 
     private void Generate()
@@ -31,12 +36,34 @@ public class TerrainGenerator : MonoBehaviour
             if (previousPositionY + yOffSet < Height + transform.position.y && previousPositionY + yOffSet > transform.position.y && previousPositionX % 3 == 0)
                 previousPositionY += yOffSet;
             var cellClone = Instantiate(cell, new Vector3(previousPositionX, previousPositionY, 0), transform.rotation, transform);
+            cells.Add(cellClone);
             previousPositionX++;
         }
         if (skater.position.x + 12.6f >= previousBGPosX)
         {
             previousBGPosX += 12.6f;
-            Instantiate(background, new Vector3(previousBGPosX, 0, 0), transform.rotation, transform);
+            var BGClone = Instantiate(background, new Vector3(previousBGPosX, 0, 0), transform.rotation, transform);
+            BGs.Add(BGClone);
+        }
+    }
+
+    private void Delete()
+    {
+        for (int i = 0; i < cells.Count; i++)
+        {
+            if (cells[i].transform.position.x < Camera.main.transform.position.x - 1600 * Camera.main.orthographicSize / 900 - 1)
+            {
+                Destroy(cells[i]);
+                cells.RemoveAt(i);
+            }
+        }
+        for (int i = 0; i < BGs.Count; i++)
+        {
+            if (BGs[i].transform.position.x < Camera.main.transform.position.x - 1600 * Camera.main.orthographicSize / 900 - 7)
+            {
+                Destroy(BGs[i]);
+                BGs.RemoveAt(i);
+            }
         }
     }
 }
